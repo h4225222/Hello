c40b729de36e6f554423afa65e881d62e369c90f
h4225222 <994667214@qq.com>
2020-09-18 16:49:53 +0800

    Add files via upload

---
+++ 工具/move.bat
@@ -0,0 +1,13 @@
+set  p1="../Assets/Scripts/application/config/table"
+copy BuffTable.cs %p1%
+copy EntityDescTable.cs %p1%
+copy GiftTable.cs %p1%
+copy SkillTable.cs %p1%
+copy TrapTable.cs %p1%
+set  p2="../Assets/Resources/config/table"
+copy BuffTable.json %p2%
+copy EntityDescTable.json %p2%
+copy GiftTable.json %p2%
+copy SkillTable.json %p2%
+copy TrapTable.json %p2%
+pause
